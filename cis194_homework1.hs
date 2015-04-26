327529ce59fc7631036452b43e54ab1f0d74afb6
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2015-04-26 15:36:09 +0200

    Update cis194_homework1.hs

--- cis194_homework1.hs
+++ cis194_homework1.hs
@@ -2,7 +2,6 @@ module Homework1
 ( validate
 , hanoi
 ) where
-import Data.List.Split
 
 -- validating credit card numbers
 
@@ -10,19 +9,19 @@ double :: Int -> Int
 double = (* 2)
 
 toDigits :: Int -> [Int]
-toDigits n = filter (> 0) (map (\x -> read [x] :: Int) (show n))
+toDigits n = filter (> 0) $ map (\x -> read [x] :: Int) $ show n
 
 toDigitsRev :: Int -> [Int]
-toDigitsRev n = reverse (toDigits n)
+toDigitsRev n = reverse $ toDigits n
 
 doubleEveryOther :: [Int] -> [Int]
-doubleEveryOther = reverse . (zipWith (*) (cycle [1, 2])) . reverse
+doubleEveryOther = reverse . (zipWith (*) $ cycle [1, 2]) . reverse
 
 sumDigits :: [Int] -> Int
 sumDigits = (foldl (+) 0) . (concatMap toDigits)
 
 validate :: Int -> Bool
-validate n = 0 == (sumDigits (doubleEveryOther (toDigits n))) `mod` 10
+validate n = 0 == (sumDigits $ doubleEveryOther $ toDigits n) `mod` 10
 
 
 -- towers of hanoi
